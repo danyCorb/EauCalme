1ed39ebf27928605691495af0746a43f9028b03f
dany corbineau <danycor2@hotmail.fr>
2018-02-13 15:02:10 +0100

    exempe test

--- src/Data/DataMain.java
+++ src/Data/DataMain.java
@@ -1,7 +1,5 @@
 package Data;
 
-import InterfaceGraphique.Fenetre;
-
 public class DataMain {
 	
 	private static DataMain INSTANCE =new DataMain();
@@ -9,16 +7,13 @@ public class DataMain {
 		return INSTANCE;
     }
 	
-	public int mouseX;
-	public int mouseY;
+	
 	
 	private DataMain(){
 		
 	}
 	
-	public void getMousePosition(int x,int y){
-		this.mouseX=x;
-		this.mouseY=y;
-	}
-
+	
+	
+	
 }
